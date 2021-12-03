9f7384f59c82f526dae905c31d8e206644ea693a
gfrancoa <gfrancoa@outlook.com>
2021-12-02 21:44:09 -0500

    migration of project from html to angular (just layout)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { NavbarDarkComponent } from './components/navbar-dark/navbar-dark.component';
+import { EditarPerfilComponent } from './components/editar-perfil/editar-perfil.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    InicioComponent,
+    InicioSesionComponent,
+    RegistroComponent,
+    NavbarDarkComponent,
+    EditarPerfilComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/navbar-dark/navbar-dark.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-dark',
+  templateUrl: './navbar-dark.component.html',
+  styleUrls: ['./navbar-dark.component.css']
+})
+export class NavbarDarkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
